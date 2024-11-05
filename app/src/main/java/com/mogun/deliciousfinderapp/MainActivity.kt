e14b7b549f13bbc223216fc0dbcf51276652b712
seungohjin <corrobolea@gmail.com>
2024-11-05 21:30:21 +0900

    update: 네이버 지도 view 세팅 및 기본 기능 연동

--- app/src/main/java/com/mogun/deliciousfinderapp/MainActivity.kt
+++ app/src/main/java/com/mogun/deliciousfinderapp/MainActivity.kt
@@ -1,17 +1,71 @@
 package com.mogun.deliciousfinderapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.mogun.deliciousfinderapp.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+    private var isMapInit = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    // 맵 객체를 얻어온 뒤에 추후 동작들이 가능하다.
+    override fun onMapReady(mapObject: NaverMap) {
+        naverMap = mapObject
+        isMapInit = true
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
+            .animate(CameraAnimation.Easing)
+        naverMap.moveCamera(cameraUpdate)
     }
 }
