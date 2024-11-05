0da0c3f6f1bd7ef793baa088165cc7d6f795efb1
seungohjin <corrobolea@gmail.com>
2024-11-05 16:09:23 +0900

    first commit

---
+++ app/src/main/java/com/mogun/deliciousfinderapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mogun.deliciousfinderapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.mogun.deliciousfinderapp.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
